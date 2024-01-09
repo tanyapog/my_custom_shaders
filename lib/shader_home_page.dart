7cdab4e9d6b79bba85e17dbe0987b64438693d07
tanyapog <megatatb@yandex.ru>
2024-01-09 19:29:24 +0300

    ShaderHomePage - a stateful widget, that repaint my shader periodically

--- lib/shader_home_page.dart
+++ lib/shader_home_page.dart
@@ -1,17 +1,48 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:ui';
 
-class ShaderHomePage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:my_custom_shaders/shader_painter.dart';
+
+class ShaderHomePage extends StatefulWidget {
   const ShaderHomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Custom shaders'),
-      ),
-      body: const Center(
-        child: Text('You have pushed the button this many times:'),
-      ),
+  State<ShaderHomePage> createState() => _ShaderHomePageState();
+}
+
+class _ShaderHomePageState extends State<ShaderHomePage> {
+  late Timer timer;
+  double delta = 0;
+  FragmentShader? shader;
+
+  void loadMyShader() async {
+    var program = await FragmentProgram.fromAsset('shaders/wavy_stripes.frag');
+    shader = program.fragmentShader();
+    setState(() {}); // trigger a repaint
+    timer = Timer.periodic(
+        const Duration(milliseconds: 16),
+        (timer) => setState(() => delta += 1/60),
     );
   }
+
+  @override
+  void initState() {
+    super.initState();
+    loadMyShader();
+  }
+
+
+  @override
+  void dispose() {
+    timer.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return shader == null
+        ? const Center(child: CircularProgressIndicator())
+        : CustomPaint(painter: ShaderPainter(shader!, delta));
+    }
 }
